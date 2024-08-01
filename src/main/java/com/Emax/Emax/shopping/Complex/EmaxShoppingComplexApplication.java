acd90918fcbe1786f9be0b7e3baff4c954c1529d
ebukizy1 <ebukizy1@gmail.com>
2024-08-01 04:43:52 +0100

    first push

---
+++ src/main/java/com/Emax/Emax/shopping/Complex/EmaxShoppingComplexApplication.java
@@ -0,0 +1,13 @@
+package com.Emax.Emax.shopping.Complex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmaxShoppingComplexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmaxShoppingComplexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Emax/Emax/shopping/Complex/EmaxShoppingComplexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Emax.Emax.shopping.Complex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmaxShoppingComplexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
